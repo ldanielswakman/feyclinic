403caaa46fdfbc047995a0a8481450584289942c
Daniel Swakman <dswakman@oefenweb.nl>
2015-03-18 01:01:58 +0200

    added google analytics

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -51,6 +51,16 @@
     <link href="favicon.ico" type="image/x-icon" rel="icon" />
     <link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-9833982-24', 'auto');
+      ga('send', 'pageview');
+    </script>
+
   </head>
   <body class="<?php echo $page->template() ?>">
 
