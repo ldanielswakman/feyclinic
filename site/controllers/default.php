a3e0971a89123446b8ca5d0d32bcd42e0cb414f0
L Daniel Swakman <d.swakman@gmail.com>
2020-11-09 09:11:47 +0100

    Updated uniform

--- site/controllers/default.php
+++ site/controllers/default.php
@@ -1,60 +1,34 @@
 <?php
+use Uniform\Form;
 
-return function($site, $pages, $page) {
+return function ($kirby) {
 
-  $alert = null;
+  $form = new Form([
+    'name' => [
+        'rules' => ['required', 'min' => 3, 'max' => 200],
+        'message' => 'Vul een geldige naam in (minimaal 3 karakters)',
+    ],
+    'email' => [
+        'rules' => ['required', 'email'],
+        'message' => 'Vul een geldig e-mailadres in',
+    ],
+    'text' => [
+        'rules' => ['required', 'min' => 3, 'max' => 3000],
+        'message' => 'Minimaal 3 en maximaal 3000 karakters toegestaan',
+    ],
+  ]);
 
-  if(get('submit')) {
-
-    $data = array(
-      'name'  => get('name'),
-      'email' => get('email'),
-      'text'  => get('text')
-    );
-
-    $rules = array(
-      'name'  => array('required', 'min' => 3),
-      'email' => array('required', 'email'),
-      'text'  => array('required', 'min' => 3, 'max' => 3000)
-    );
-
-    $messages = array(
-      'name'  => 'Vul een geldige naam in (minimaal 3 karakters)',
-      'email' => 'Vul een geldig e-mailadres in',
-      'text'  => 'Minimaal 3 en maximaal 3000 karakters toegestaan'
-    );
-
-    // some of the data is invalid
-    if($invalid = invalid($data, $rules, $messages)) {
-      $alert = $invalid;
-    // the data is fine, let's send the email
-    } else {
-
-      // create the body from a simple snippet
-      $body  = snippet('contactmail', $data, true);
-
-      // build the email
-      $email = email(array(
-        'to'      => 'info@feyclinic.nl',
-        'from'    => 'Feyclinic-Contact@feyclinic.nl',
-        'subject' => 'Informatieverzoek van ' . $data['name'],
-        'replyTo' => $data['email'],
-        'body'    => $body
-      ));
-
-      // try to send it and redirect to the
-      // 'sent' status if it worked
-      if($email->send()) {
-        go('/contact/status:sent#contact');
-      // add the error to the alert list if it failed
-      } else {
-        $alert = array($email->error());
-      }
-
-    }
-
+  if ($kirby->request()->is('POST')) {
+      $form->emailAction([
+          'to' => 'hello@ldaniel.eu',
+          // 'to' => 'info@feyclinic.nl',
+          'from' => 'feyclinic-contact@feyclinic.nl',
+      ])
+      ->logAction([
+          'file' => kirby()->roots()->site().'/messages.log'
+      ]);;
   }
 
-  return compact('alert');
+  return compact('form');
 
 };
--- site/templates/contact-form.php
+++ site/templates/contact-form.php
@@ -4,43 +4,36 @@ if ($pages->find('contactformulier')) :
 <div class="row u-mt20">
 	<div class="col-sm-8 col-sm-offset-2">
   
-		<form method="post" action="<?php echo $page->url() ?>/#contact">
+		<form method="post" action="<?= $page->url() ?>/#contact-form">
 
-      <?php if($alert): ?>
-      <div class="message alert u-pa20">
-        <i class="ion ion-alert-circled ion-2x u-floatleft u-mr20 u-mb20"></i>
-        <p><?php echo $pages->find('contactformulier')->message_alert() ?></p>
-        <ul>
-          <?php foreach($alert as $message): ?>
-          <li><?php echo html($message) ?></li>
-          <?php endforeach ?>
-        </ul>
-      </div>
-      <?php endif ?>
-
-      <?php if(param('status') == 'sent'): ?>
+      <?php if($form->success()): ?>
 
       <div class="message success u-pa20">
         <i class="ion ion-checkmark ion-2x u-floatleft u-mr20"></i>
-        <p><?php echo $pages->find('contactformulier')->message_success() ?></p>
+        <p><?= $pages->find('contactformulier')->message_success() ?></p>
       </div>
 
       <?php else : ?>
+
+      <?php snippet('uniform/errors', ['form' => $form]); ?>
         
       <div class="form-group">
         <label for="text">Hallo, ik wil graag meer weten over...</label>
-        <textarea class="form-control" id="text" name="text" rows="5" value="" placeholder="Uw bericht of verzoek tot afspraak" required></textarea>
+        <textarea class="form-control" id="text" name="text" rows="5" value="<?= $form->old('text') ?>" placeholder="Uw bericht of verzoek tot afspraak" required></textarea>
       </div>
 
       <div class="form-group">
         <label for="name">Mijn naam is ...</label>
-        <input class="form-control" type="text" id="name" name="name" value="" placeholder="Uw naam">
+        <input class="form-control" type="text" id="name" name="name" value="<?= $form->old('name') ?>" placeholder="Uw naam">
       </div>
 
       <div class="form-group">
         <label for="email">U kunt mij bereiken via ...</label>
-        <input class="form-control" type="email" id="email" name="email" value="" placeholder="Uw e-mailadres" required>
+        <input class="form-control" type="email" id="email" name="email" value="<?= $form->old('email') ?>" placeholder="Uw e-mailadres" required>
       </div>
+
+      <?= csrf_field(); ?>
+      <?= honeypot_field(); ?>
 
       <div class="form-group u-aligncenter">
       	<input class="btn btn-primary btn-lg" type="submit" name="submit" value="Verzenden">
